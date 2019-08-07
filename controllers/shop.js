c4bf91606d910db7ff08335e0ce9a450d333f15e
Rene Cruz <renecruzATX@outlook.com>
2019-08-07 13:25:01 -0500

    Adjust for adding and deleting items from the cart with mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,8 +44,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
     req.user
-        .getCart()
-        .then(products => {            
+        .populate('cart.items.productId')
+        .execPopulate()
+        .then(user => {       
+            const products = user.cart.items;     
             res.render('shop/cart', {
                 path: '/cart',
                 pageTitle: 'Shopping Cart',
@@ -70,7 +72,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     req.user
-        .deleteItemFromCart(prodId)        
+        .removeFromCart(prodId)        
         .then(result => {
             res.redirect('/cart');
         })
