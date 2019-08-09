517ab3e810da5d43c08696ca69c162bb470f84fc
Rene Cruz <renecruzATX@outlook.com>
2019-08-09 15:39:12 -0500

    Protected routes with custom middleware and added CSRF protection

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, next) => {
+exports.getAddProduct = (req, res, next) => {    
     res.render('admin/edit-product', { 
       pageTitle: 'Add Product', 
       path: '/admin/add-product',
--- controllers/shop.js
+++ controllers/shop.js
@@ -37,8 +37,7 @@ exports.getIndex = (req, res, next) => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/',
-                isAuthenticated: req.session.isLoggedIn
+                path: '/'
             });
         }) 
         .catch(err => {
@@ -95,7 +94,7 @@ exports.postOrder = (req, res, next) => {
             }); 
             const order = new Order({
                 user: {
-                    name: req.user.name,
+                    email: req.user.email,
                     userId: req.user
                 },
                 products: products
