72f3c17c879b6299e895ea3161232ca578875fa1
Rene Cruz <renecruzATX@outlook.com>
2019-08-09 10:13:08 -0500

    Added Cookie for login page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req
+        .get('Cookie')
+        .split('=')[1]
+        .trim();
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
     });   
+};
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'isLoggedIn = true');
+    res.redirect('/');   
 };
