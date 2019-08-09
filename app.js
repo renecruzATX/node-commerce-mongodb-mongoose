111851dce0f7c04259efd3a18880537925f75afc
Rene Cruz <renecruzATX@outlook.com>
2019-08-09 11:15:51 -0500

    Added cookies and sessions to login process

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb://tester:test1234@ds143449.mlab.com:43449/node-commerce-mongodb-mongoose';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +26,12 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'total secret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: store
+}));
 
 app.use((req, res, next) => {
   User.findById('5d4b09ce1668a24a1b0d5415')
@@ -35,7 +49,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb://tester:test1234@ds143449.mlab.com:43449/node-commerce-mongodb-mongoose', { useNewUrlParser: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,17 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req
-        .get('Cookie')
-        .split('=')[1]
-        .trim();
+    // const isLoggedIn = req
+    //     .get('Cookie')
+    //     .split('=')[1]
+    //     .trim();
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });   
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'isLoggedIn = true');
+    req.session.isLoggedIn = true;
     res.redirect('/');   
 };
