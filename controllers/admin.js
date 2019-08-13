e423a0d218492602f39997cf0360c4337e1a734b
Rene <renecruzatx@outlook.com>
2019-08-13 15:01:30 -0500

    Added authentication

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,21 +62,25 @@ exports.postEditProduct = (req, res, next) => {
   
   Product.findById(prodId)
     .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.imageUrl = updatedImageUrl;
       product.description = updatedDesc;
-      return product.save();
-    })
-    .then(result => {
-      console.log('Updated Product');
-      res.redirect('/admin/products');
-    })
+      return product
+        .save()
+          .then(result => {
+            console.log('Updated Product');
+            res.redirect('/admin/products');
+          });
+    })    
     .catch(err => console.log(err));  
 };
 
 exports.getProducts = (req, res, next) => { 
-  Product.find()
+  Product.find({userId: req.user._id})
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -90,7 +94,7 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId; 
   Product
-    .findByIdAndRemove(prodId)
+    .deleteOne({_id: prodId, userId: req.user._id})
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products'); 
