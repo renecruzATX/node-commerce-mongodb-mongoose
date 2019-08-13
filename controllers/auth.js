6330c60e523839aa5ea5b9a1b6e0a868ebcdab5a
Rene <renecruzatx@outlook.com>
2019-08-13 15:33:14 -0500

    Added server side validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -75,7 +76,18 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
+    //const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array);
+        return res
+            .status(422)
+            .render('auth/signup', {
+                path: '/signup',
+                pageTitle: 'Signup',
+                errorMessage: errors.array()
+            });
+    }
     User.findOne({email: email})
         .then(userDoc => {
             if (userDoc) {
