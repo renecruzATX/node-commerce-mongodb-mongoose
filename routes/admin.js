b06eddb34d8e5ab08133faa6e67d3f76cc64368a
Rene <renecruzatx@outlook.com>
2019-08-15 10:07:10 -0500

    Added validation for adding and editing products

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,37 @@
 const path = require('path');
 
 const express = require('express');
+const {body} = require('express-validator');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
+const validate = [
+    body('title')
+        .isString()
+        .isLength({min: 3})
+        .trim(),
+    body('imageUrl')
+        .isURL(),
+    body('price')
+        .isFloat(),
+    body('description')
+        .isLength({min: 5, max: 400})
+        .trim()
+];
  
-router.get('/add-product', isAuth, adminController.getAddProduct);
+router.get(
+    '/add-product', validate, isAuth, adminController.getAddProduct);
 
 router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product', validate, isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct); 
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product', validate, isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
